05cc674ec3ebdf90069dbdf23cbeccbf60e0cca4
thais-reiss <thaisthatareis@gmail.com>
2025-11-21 15:21:30 -0300

    feat: função que gerencia as cores e varre o terreno

--- flood_fill.py
+++ flood_fill.py
@@ -27,3 +27,26 @@ def flood_fill_recursive(grid, x, y, new_color):
     for dx, dy in directions:
         new_x, new_y = x + dx, y + dy
         flood_fill_recursive(grid, new_x, new_y, new_color)
+
+
+def color_all_regions(grid, start_x, start_y):
+    """
+    Função que gerencia as cores e analisa o terreno.
+    """
+    current_color = 2 
+    
+    print(f"Iniciando pintura na coordenada ({start_x}, {start_y}) com a cor {current_color}...")
+
+    if is_valid(grid, start_x, start_y) and grid[start_x][start_y] == 0:
+        flood_fill_recursive(grid, start_x, start_y, current_color)
+        current_color += 1 
+    else:
+        print("A coordenada inicial não é válida ou é um obstáculo.")
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 0: 
+                flood_fill_recursive(grid, i, j, current_color)
+                current_color += 1 
+    
+    return grid
